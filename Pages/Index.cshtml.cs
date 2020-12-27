7a03f2302ca0695436dbf2a11ae53a0783b888b0
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-12-27 21:36:23 +0330

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using RMuseum.Models.Ganjoor.ViewModels;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GanjooRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private List<GanjoorPoetViewModel> _poets;
+        public List<GanjoorPoetViewModel> Potes { get { return _poets; } }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://ganjgah.ir/api/ganjoor/poets");
+                response.EnsureSuccessStatusCode();
+
+                _poets = JArray.Parse(await response.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+            }
+        }
+    }
+}
