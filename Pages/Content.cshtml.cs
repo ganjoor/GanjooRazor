d0f4a56c5abaf603f7f353c9422bc90ad1dc62e6
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2020-12-29 19:53:32 +0330

    displaying category pages

--- Pages/Content.cshtml.cs
+++ Pages/Content.cshtml.cs
@@ -12,22 +12,45 @@ namespace GanjooRazor.Pages
         private List<GanjoorPoetViewModel> _poets;
         public List<GanjoorPoetViewModel> Poets { get { return _poets; } }
 
-        public GanjoorPoetCompleteViewModel Poet { get; set; }
+        public GanjoorPoetCompleteViewModel Cat { get; set; }
+
+        public bool PoetPage { get; set; }
+
+        public bool CatPage { get; set; }
+
 
         public async Task OnGet()
         {
+            PoetPage = false;
+            CatPage = false;
             using (HttpClient client = new HttpClient())
             {
-
-                var poetQuery = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/poet?url={Request.Path}");
-                poetQuery.EnsureSuccessStatusCode();
-
-                Poet = JObject.Parse(await poetQuery.Content.ReadAsStringAsync()).ToObject<GanjoorPoetCompleteViewModel>();
 
                 var response = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/poets?includeBio=false");
                 response.EnsureSuccessStatusCode();
 
                 _poets = JArray.Parse(await response.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+
+
+                var poetQuery = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/poet?url={Request.Path}");
+                
+
+                if(poetQuery.IsSuccessStatusCode)
+                {
+                    Cat = JObject.Parse(await poetQuery.Content.ReadAsStringAsync()).ToObject<GanjoorPoetCompleteViewModel>();
+                    PoetPage = true;
+                }
+                else
+                    if(poetQuery.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        var catQuery = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/cat?url={Request.Path}");
+                        if (catQuery.IsSuccessStatusCode)
+                        {
+                            Cat = JObject.Parse(await catQuery.Content.ReadAsStringAsync()).ToObject<GanjoorPoetCompleteViewModel>();
+                            CatPage = true;
+                        }
+                    }
+
             }
         }
     }
