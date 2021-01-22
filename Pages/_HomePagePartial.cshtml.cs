99ba53be527a1b62b94e74466406bd6c45c09aac
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-01-22 15:01:48 +0330

    #7 moving inline login to a separate partial page

---
+++ Pages/_HomePagePartial.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GanjooRazor.Pages
+{
+    public class _HomePagePartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
