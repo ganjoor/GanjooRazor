26a2cf71eff569f546e4cbdd8905207aa01353e0
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-02-01 18:54:12 +0330

    #7 review song form now displays related poem info and ...

--- Areas/User/Pages/ReviewSongs.cshtml.cs
+++ Areas/User/Pages/ReviewSongs.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using RMuseum.Models.Ganjoor.ViewModels;
+using RMuseum.Services.Implementation.ImportedFromDesktopGanjoor;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -22,6 +23,11 @@ namespace GanjooRazor.Areas.User.Pages
         public PoemMusicTrackViewModel PoemMusicTrackViewModel { get; set; }
 
         /// <summary>
+        /// poem model
+        /// </summary>
+        public GanjoorPoemCompleteViewModel Poem { get; set; }
+
+        /// <summary>
         /// get
         /// </summary>
         public async Task OnGetAsync()
@@ -31,15 +37,29 @@ namespace GanjooRazor.Areas.User.Pages
             {
                 if (await GanjoorSessionChecker.PrepareClient(client, Request, Response))
                 {
-                    var methodUrl = $"{APIRoot.Url}/api/ganjoor/song?skip=0&onlyMine=false";
-                    var response = await client.GetAsync(methodUrl);
-                    if (!response.IsSuccessStatusCode)
+                    var trackResponse = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/song?skip=0&onlyMine=false");
+                    if (!trackResponse.IsSuccessStatusCode)
                     {
-                        LastError = await response.Content.ReadAsStringAsync();
+                        LastError = await trackResponse.Content.ReadAsStringAsync();
                     }
                     else
                     {
-                        PoemMusicTrackViewModel = JsonConvert.DeserializeObject<PoemMusicTrackViewModel>(await response.Content.ReadAsStringAsync());
+                        PoemMusicTrackViewModel = JsonConvert.DeserializeObject<PoemMusicTrackViewModel>(await trackResponse.Content.ReadAsStringAsync());
+
+                        PoemMusicTrackViewModel.ArtistName = GPersianTextSync.Sync(PoemMusicTrackViewModel.ArtistName);
+                        PoemMusicTrackViewModel.AlbumName = GPersianTextSync.Sync(PoemMusicTrackViewModel.AlbumName);
+                        PoemMusicTrackViewModel.TrackName = GPersianTextSync.Sync(PoemMusicTrackViewModel.TrackName);
+
+
+                        var poemResponse = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/poem/{PoemMusicTrackViewModel.PoemId}?catInfo=false&rhymes=false&recitations=false&images=false&songs=false&comments=false&verseDetails=false&navigation=false");
+                        if (poemResponse.IsSuccessStatusCode)
+                        {
+                            Poem = JsonConvert.DeserializeObject<GanjoorPoemCompleteViewModel>(await poemResponse.Content.ReadAsStringAsync());
+                        }
+                        else
+                        {
+                            LastError = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                        }
                     }
                 }
                 else
@@ -48,6 +68,16 @@ namespace GanjooRazor.Areas.User.Pages
                 }
 
             }
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            PoemMusicTrackViewModel.Approved = Request.Form["approve"].Count == 1;
+
+            await OnGetAsync();
+
+            return Page();
         }
     }
 }
