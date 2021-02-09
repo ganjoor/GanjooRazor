32ae139b4e55161fb42e8241639ec9879bbfeac6
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-02-09 21:00:08 +0330

    #11 صفحهٔ حاشیه ها

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,9 +8,11 @@ using RMuseum.Models.Ganjoor.ViewModels;
 using RMuseum.Models.GanjoorAudio.ViewModels;
 using RMuseum.Services.Implementation.ImportedFromDesktopGanjoor;
 using RSecurityBackend.Models.Auth.ViewModels;
+using RSecurityBackend.Models.Generic;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -332,7 +334,12 @@ namespace GanjooRazor.Pages
                     {
                         using (HttpClient client = new HttpClient())
                         {
-                            var response = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/comments?PageNumber=1&PageSize=20");
+                            int pageNumber = 1;
+                            if(!string.IsNullOrEmpty(Request.Query["page"]))
+                            {
+                                pageNumber = int.Parse(Request.Query["page"]);
+                            }
+                            var response = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/comments?PageNumber={pageNumber}&PageSize=20");
                             response.EnsureSuccessStatusCode();
 
                             string htmlText = "";
@@ -347,6 +354,44 @@ namespace GanjooRazor.Pages
                                     $"<p>{comment.AuthorName} <small>در تاریخ {GPersianTextSync.Sync(dateInShamsi)} ساعت {GPersianTextSync.Sync($"{comment.CommentDate:HH:mm}")}</small> دربارهٔ <a href=\"{comment.Poem.UrlSlug}\">{comment.Poem.Title}</a>" +
                                     $"</blockquote>{Environment.NewLine}<hr />{Environment.NewLine}";
                             }
+
+                            htmlText += "<p style=\"text-align: center;\">";
+
+                            string paginnationMetadata = response.Headers.GetValues("paging-headers").FirstOrDefault();
+                            if (!string.IsNullOrEmpty(paginnationMetadata))
+                            {
+                                PaginationMetadata paginationMetadata = JsonConvert.DeserializeObject<PaginationMetadata>(paginnationMetadata);
+
+                                if(paginationMetadata.totalPages > 1)
+                                {
+                                    if(paginationMetadata.currentPage > 3)
+                                    {
+                                        htmlText += $"[<a href=\"/hashieha/?page=1\">صفحهٔ اول</a>] …";
+                                    }
+                                    for (int i = (paginationMetadata.currentPage - 2); i <= (paginationMetadata.currentPage + 2); i++)
+                                    {
+                                        if(i >= 1 && i<= paginationMetadata.totalPages)
+                                        {
+                                            htmlText += " [";
+                                            if (i == paginationMetadata.currentPage)
+                                            {
+                                                htmlText += GPersianTextSync.Sync(i.ToString());
+                                            }
+                                            else
+                                            {
+                                                htmlText += $"<a href=\"/hashieha/?page={i}\">{GPersianTextSync.Sync(i.ToString())}</a>";
+                                            }
+                                            htmlText += "] ";
+                                        }
+                                    }
+                                    if(paginationMetadata.totalPages > (paginationMetadata.currentPage + 2) )
+                                    {
+                                        htmlText += $"… [<a href=\"/hashieha/?page={paginationMetadata.totalPages}\">صفحهٔ آخر</a>]";
+                                    }
+                                }
+                            }
+
+                            htmlText += $"</p>{Environment.NewLine}";
 
                             GanjoorPage.HtmlText = htmlText;
                         }
