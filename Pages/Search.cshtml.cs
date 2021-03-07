b958f5e9ac9bb3e5f82b905af7cb50382c09d606
Iman Abidi <imanabidi@gmail.com>
2021-03-07 19:40:32 +0100

    #23 search verses paging feature

--- Pages/Search.cshtml.cs
+++ Pages/Search.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using DNTPersianUtils.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RMuseum.Models.Ganjoor.ViewModels;
+using RSecurityBackend.Models.Generic;
 
 namespace GanjooRazor.Pages
 {
@@ -23,6 +26,9 @@ namespace GanjooRazor.Pages
 
         public List<GanjoorSearchVerseViewModel> Verses { get; set; }
 
+        public string PagingToolsHtml { get; set; }
+
+
         public async Task<IActionResult> OnGet()
         {
             Query = Request.Query["s"];
@@ -30,17 +36,32 @@ namespace GanjooRazor.Pages
 
             using (HttpClient client = new HttpClient())
             {
-                var response = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/poets?includeBio=false");
-                response.EnsureSuccessStatusCode();
+                //todo: use html master layout or make it partial
+                // 1. poets 
+                var poetsResponse = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/poets?includeBio=false");
+                poetsResponse.EnsureSuccessStatusCode();
+                Poets = JArray.Parse(await poetsResponse.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+                var poetName = Poets.SingleOrDefault(p => p.Id == AuthorId);
+                if (poetName != null)
+                {
+                    ViewData["Title"] = $"گنجور &raquo; نتایج جستجو برای {Query} &raquo; {poetName?.Name}";
+                }
+                else
+                {
+                    ViewData["Title"] = $"گنجور &raquo; نتایج جستجو برای {Query}";
+                }
 
-                Poets = JArray.Parse(await response.Content.ReadAsStringAsync()).ToObject<List<GanjoorPoetViewModel>>();
+                // 2. search verses
+                int pageNumber = 1;
+                if (!string.IsNullOrEmpty(Request.Query["page"]))
+                {
+                    pageNumber = int.Parse(Request.Query["page"]);
+                }
 
+                var searchQueryResponse = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/verse/search?query={Query}&poetId={AuthorId}&PageNumber={pageNumber}&PageSize=20");
+                searchQueryResponse.EnsureSuccessStatusCode();
 
-                var searchQueryResult = await client.GetAsync($"{APIRoot.Url}/api/ganjoor/verse/search?query={Query}&poetId={AuthorId}");
-                searchQueryResult.EnsureSuccessStatusCode();
-
-                Verses = JArray.Parse(await searchQueryResult.Content.ReadAsStringAsync()).ToObject<List<GanjoorSearchVerseViewModel>>();
-
+                Verses = JArray.Parse(await searchQueryResponse.Content.ReadAsStringAsync()).ToObject<List<GanjoorSearchVerseViewModel>>();
                 if (Verses != null)
                 {
                     // highlight searched word
@@ -48,20 +69,55 @@ namespace GanjooRazor.Pages
                     {
                         ganjoorSearchVerseViewModel.Text = Regex.Replace(ganjoorSearchVerseViewModel.Text, Query, $"<span class=\"hilite\">{Query}</span>", RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
                     }
+
+                    string paginationMetadata = searchQueryResponse.Headers.GetValues("paging-headers").FirstOrDefault();
+
+                    PagingToolsHtml = GeneratePagingBarHtml(paginationMetadata, $"/search?s={Query}&author={AuthorId}");
                 }
-            }
-            
-            var poetName = Poets.SingleOrDefault(p => p.Id == AuthorId);
-            if (poetName != null)
-            {
-                ViewData["Title"] = $"گنجور &raquo; نتایج جستجو برای {Query} &raquo; {poetName?.Name}";
-            }
-            else
-            {
-                ViewData["Title"] = $"گنجور &raquo; نتایج جستجو برای {Query}";
             }
 
             return Page();
+        }
+
+        private string GeneratePagingBarHtml(string paginationMetadataJsonValue, string routeStartWithQueryStrings)
+        {
+            string htmlText = "<p style=\"text-align: center;\">";
+
+            if (!string.IsNullOrEmpty(paginationMetadataJsonValue))
+            {
+                PaginationMetadata paginationMetadata = JsonConvert.DeserializeObject<PaginationMetadata>(paginationMetadataJsonValue);
+
+                if (paginationMetadata.totalPages > 1)
+                {
+                    if (paginationMetadata.currentPage > 3)
+                    {
+                        htmlText += $"[<a href=\"{routeStartWithQueryStrings}&page=1\">صفحهٔ اول</a>] …";
+                    }
+                    for (int i = (paginationMetadata.currentPage - 2); i <= (paginationMetadata.currentPage + 2); i++)
+                    {
+                        if (i >= 1 && i <= paginationMetadata.totalPages)
+                        {
+                            htmlText += " [";
+                            if (i == paginationMetadata.currentPage)
+                            {
+                                htmlText += i.ToPersianNumbers();
+                            }
+                            else
+                            {
+                                htmlText += $"<a href=\"{routeStartWithQueryStrings}&page={i}\">{i.ToPersianNumbers()}</a>";
+                            }
+                            htmlText += "] ";
+                        }
+                    }
+                    if (paginationMetadata.totalPages > (paginationMetadata.currentPage + 2))
+                    {
+                        htmlText += $"… [<a href=\"{routeStartWithQueryStrings}&page={paginationMetadata.totalPages}\">صفحهٔ آخر</a>]";
+                    }
+                }
+            }
+
+            htmlText += $"</p>{Environment.NewLine}";
+            return htmlText;
         }
 
     }
