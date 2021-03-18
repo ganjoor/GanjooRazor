db6871da28b805e6614340cd890f5ddb63b10dca
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-03-18 16:53:30 +0330

    #27 poet images from api

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -429,7 +429,7 @@ namespace GanjooRazor.Pages
             if (IsPoetPage)
             {
                 ViewData["Title"] = $"گنجور &raquo; {GanjoorPage.PoetOrCat.Poet.Name}";
-                breadCrumbList.AddItem(GanjoorPage.PoetOrCat.Poet.Name, GanjoorPage.PoetOrCat.Cat.FullUrl, $"/image/poets/{GanjoorPage.PoetOrCat.Poet.Id}.png");
+                breadCrumbList.AddItem(GanjoorPage.PoetOrCat.Poet.Name, GanjoorPage.PoetOrCat.Cat.FullUrl, $"{GanjooRazor.APIRoot.Url+GanjoorPage.PoetOrCat.Poet.ImageUrl}");
             }
             else
             if (IsCatPage)
@@ -439,7 +439,7 @@ namespace GanjooRazor.Pages
                 foreach (var gran in GanjoorPage.PoetOrCat.Cat.Ancestors)
                 {
                     title += $"{gran.Title} &raquo; ";
-                    breadCrumbList.AddItem(gran.Title, gran.FullUrl, poetCat ? $"/image/poets/{GanjoorPage.PoetOrCat.Poet.Id}.png" : "https://i.ganjoor.net/cat.png");
+                    breadCrumbList.AddItem(gran.Title, gran.FullUrl, poetCat ? $"{GanjooRazor.APIRoot.Url+GanjoorPage.PoetOrCat.Poet.ImageUrl}" : "https://i.ganjoor.net/cat.png");
                     poetCat = false;
                 }
                 breadCrumbList.AddItem(GanjoorPage.PoetOrCat.Cat.Title, GanjoorPage.PoetOrCat.Cat.FullUrl, "https://i.ganjoor.net/cat.png");
@@ -453,7 +453,7 @@ namespace GanjooRazor.Pages
                 bool poetCat = true;
                 foreach (var gran in GanjoorPage.Poem.Category.Cat.Ancestors)
                 {
-                    breadCrumbList.AddItem(gran.Title, gran.FullUrl, poetCat ? $"/image/poets/{GanjoorPage.Poem.Category.Poet.Id}.png" : "https://i.ganjoor.net/cat.png");
+                    breadCrumbList.AddItem(gran.Title, gran.FullUrl, poetCat ? $"{GanjooRazor.APIRoot.Url+GanjoorPage.PoetOrCat.Poet.ImageUrl}" : "https://i.ganjoor.net/cat.png");
                     poetCat = false;
                 }
                 breadCrumbList.AddItem(GanjoorPage.PoetOrCat.Cat.Title, GanjoorPage.PoetOrCat.Cat.FullUrl, "https://i.ganjoor.net/cat.png");
@@ -473,12 +473,12 @@ namespace GanjooRazor.Pages
                     else
                         foreach (var gran in GanjoorPage.PoetOrCat.Cat.Ancestors)
                         {
-                            breadCrumbList.AddItem(gran.Title, gran.FullUrl, poetCat ? $"/image/poets/{GanjoorPage.Poem.Category.Poet.Id}.png" : "https://i.ganjoor.net/cat.png");
+                            breadCrumbList.AddItem(gran.Title, gran.FullUrl, poetCat ? $"{GanjooRazor.APIRoot.Url+GanjoorPage.PoetOrCat.Poet.ImageUrl}" : "https://i.ganjoor.net/cat.png");
                             poetCat = false;
                             fullTitle += $"{gran.Title} &raquo; ";
                         }
                     ViewData["Title"] = $"{fullTitle}{GanjoorPage.Title}";
-                    breadCrumbList.AddItem(GanjoorPage.PoetOrCat.Poet.Name, GanjoorPage.PoetOrCat.Cat.FullUrl, $"/image/poets/{GanjoorPage.PoetOrCat.Poet.Id}.png");
+                    breadCrumbList.AddItem(GanjoorPage.PoetOrCat.Poet.Name, GanjoorPage.PoetOrCat.Cat.FullUrl, $"{GanjooRazor.APIRoot.Url+GanjoorPage.PoetOrCat.Poet.ImageUrl}");
                 }
                 else
                 {
