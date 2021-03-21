34da4a11c975baec0e09e7f7d6cf92f1119c2bf9
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-03-21 18:10:51 +0330

    #29 saving user edited information

--- Areas/User/Pages/Index.cshtml.cs
+++ Areas/User/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using RSecurityBackend.Models.Auth.ViewModels;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GanjooRazor.Areas.Panel.Pages
@@ -56,10 +57,47 @@ namespace GanjooRazor.Areas.Panel.Pages
                     }
                     else
                     {
-                        LastError = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                        LastError = await userInfoResponse.Content.ReadAsStringAsync();
                     }
                 }
+                else
+                {
+                    LastError = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
             }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            LastError = "";
+            using (HttpClient client = new HttpClient())
+            {
+                if (await GanjoorSessionChecker.PrepareClient(client, Request, Response))
+                {
+                    var isAdminResponse = await client.GetAsync($"{APIRoot.Url}/api/users/isadmin?userId={Request.Cookies["UserId"]}");
+                    if (isAdminResponse.IsSuccessStatusCode)
+                    {
+                        UserInfo.IsAdmin = JsonConvert.DeserializeObject<bool>(await isAdminResponse.Content.ReadAsStringAsync());
+
+                        var putResponse = await client.PutAsync($"{APIRoot.Url}/api/users/{Request.Cookies["UserId"]}", new StringContent(JsonConvert.SerializeObject(UserInfo), Encoding.UTF8, "application/json"));
+
+                        if(!putResponse.IsSuccessStatusCode)
+                        {
+                            LastError = await putResponse.Content.ReadAsStringAsync();
+                        }
+                    }
+                    else
+                    {
+                        LastError = await isAdminResponse.Content.ReadAsStringAsync();
+                    }
+                }
+                else
+                {
+                    LastError = "لطفا از گنجور خارج و مجددا به آن وارد شوید.";
+                }
+            }
+
+            return Page();
         }
     }
 }
