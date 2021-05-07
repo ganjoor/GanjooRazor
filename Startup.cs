c0365236d4ce922516df52cf59bba6e1ae204165
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2021-05-07 11:31:21 +0430

    customizing generic error message

--- Startup.cs
+++ Startup.cs
@@ -33,14 +33,7 @@ namespace GanjooRazor
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+            app.UseExceptionHandler("/Error");
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
